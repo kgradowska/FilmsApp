b57f40fdaffc8bba28ae242c0b61cdf0bf6c5477
K. Gradowska <katarzyna.a.gradowska@gmail.com>
2022-09-09 21:34:07 +0200

    [feature/favourite-movies]: Saving favourite movies to shared preferences

--- app/src/main/java/gradowska/katarzyna/filmsapp/presentation/recyclerList/MoviesFragmentViewModel.kt
+++ app/src/main/java/gradowska/katarzyna/filmsapp/presentation/recyclerList/MoviesFragmentViewModel.kt
@@ -1,12 +1,18 @@
 package gradowska.katarzyna.filmsapp.presentation.recyclerList
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import gradowska.katarzyna.filmsapp.R
 import gradowska.katarzyna.filmsapp.data.MovieDataSource
+import gradowska.katarzyna.filmsapp.data.UserDataSource
 import gradowska.katarzyna.filmsapp.domain.GetMoviesUseCase
+import gradowska.katarzyna.filmsapp.domain.SetFavouriteMovieUseCase
 import gradowska.katarzyna.filmsapp.presentation.movie.MovieDataModel
 
-class MoviesFragmentViewModel : ViewModel() {
+class MoviesFragmentViewModel(application: Application) : AndroidViewModel(application) {
+    private val setFavouriteMovieUseCase: SetFavouriteMovieUseCase =
+        SetFavouriteMovieUseCase(UserDataSource(context = getApplication<Application>().applicationContext))
     private val movieList = GetMoviesUseCase(MovieDataSource()).getMoviesList()
 
     fun getMoviesList(): List<MovieDataModel> {
@@ -17,6 +23,7 @@ class MoviesFragmentViewModel : ViewModel() {
         for (m in movieList) {
             if (m.movieID == movie.movieID) {
                 m.movieLiked = !m.movieLiked
+                setFavouriteMovieUseCase.setMovieIsFavourite(m.movieID, m.movieLiked)
             }
         }
     }
