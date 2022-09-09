1ee1d5f3605e588155bfc93521a837f1257046f9
K. Gradowska <katarzyna.a.gradowska@gmail.com>
2022-09-09 21:53:25 +0200

    [feature/favourite-movies]: Getting favourite movies from shared preferences

--- app/src/main/java/gradowska/katarzyna/filmsapp/presentation/recyclerList/MoviesFragmentViewModel.kt
+++ app/src/main/java/gradowska/katarzyna/filmsapp/presentation/recyclerList/MoviesFragmentViewModel.kt
@@ -2,10 +2,9 @@ package gradowska.katarzyna.filmsapp.presentation.recyclerList
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import gradowska.katarzyna.filmsapp.R
 import gradowska.katarzyna.filmsapp.data.MovieDataSource
 import gradowska.katarzyna.filmsapp.data.UserDataSource
+import gradowska.katarzyna.filmsapp.domain.GetFavouriteMovieUseCase
 import gradowska.katarzyna.filmsapp.domain.GetMoviesUseCase
 import gradowska.katarzyna.filmsapp.domain.SetFavouriteMovieUseCase
 import gradowska.katarzyna.filmsapp.presentation.movie.MovieDataModel
@@ -13,7 +12,22 @@ import gradowska.katarzyna.filmsapp.presentation.movie.MovieDataModel
 class MoviesFragmentViewModel(application: Application) : AndroidViewModel(application) {
     private val setFavouriteMovieUseCase: SetFavouriteMovieUseCase =
         SetFavouriteMovieUseCase(UserDataSource(context = getApplication<Application>().applicationContext))
-    private val movieList = GetMoviesUseCase(MovieDataSource()).getMoviesList()
+
+    private val getFavouriteMovieUseCase: GetFavouriteMovieUseCase =
+        GetFavouriteMovieUseCase(UserDataSource(context = getApplication<Application>().applicationContext))
+
+    private val movieList =
+        GetMoviesUseCase(MovieDataSource(), getFavouriteMovieUseCase).getMoviesList()
+
+//    init {
+//        updateMoviesFavouriteStatus()
+//    }
+//
+//    private fun updateMoviesFavouriteStatus() {
+//        for (m in movieList) {
+//            m.movieLiked = getFavouriteMovieUseCase.getMovieIsFavourite(m.movieID)
+//        }
+//    }
 
     fun getMoviesList(): List<MovieDataModel> {
         return movieList
