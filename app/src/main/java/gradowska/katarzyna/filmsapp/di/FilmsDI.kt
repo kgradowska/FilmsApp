c764b7eaa4e5ccd1214970436eb7f5f2f14ab4a7
K. Gradowska <katarzyna.a.gradowska@gmail.com>
2024-12-05 11:50:07 +0100

    [feature/genre]: kapt changed for ksp, libraries versions bumped

--- app/src/main/java/gradowska/katarzyna/filmsapp/di/FilmsDI.kt
+++ app/src/main/java/gradowska/katarzyna/filmsapp/di/FilmsDI.kt
@@ -15,7 +15,7 @@ import gradowska.katarzyna.filmsapp.presentation.start.StartFragmentViewModel
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.*
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
