4814a9cb6db1d19e94e8f753748aeed5685783d8
K. Gradowska <katarzyna.a.gradowska@gmail.com>
2025-04-19 23:29:26 +0200

    [feature/tests]: test for GetMoviesGenresUseCase added

--- app/src/main/java/gradowska/katarzyna/filmsapp/domain/usecase/SetFavouriteMovieUseCase.kt
+++ app/src/main/java/gradowska/katarzyna/filmsapp/domain/usecase/SetFavouriteMovieUseCase.kt
@@ -2,7 +2,7 @@ package gradowska.katarzyna.filmsapp.domain.usecase
 
 import gradowska.katarzyna.filmsapp.data.UserDataSource
 
-class SetFavouriteMovieUseCase(val userDataSource: UserDataSource) {
+class SetFavouriteMovieUseCase(private val userDataSource: UserDataSource) {
 
     fun setMovieIsFavourite(movieId: String, isFavourite: Boolean) =
         userDataSource.setMovieIsFavourite(movieId, isFavourite)
