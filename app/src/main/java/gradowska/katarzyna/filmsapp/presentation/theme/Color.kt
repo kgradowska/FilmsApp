8316655df035d684cf8e192dfd9250fc8b351cac
K. Gradowska <katarzyna.a.gradowska@gmail.com>
2026-02-08 20:11:31 +0100

    [feature/compose]: add compose color definitions

---
+++ app/src/main/java/gradowska/katarzyna/filmsapp/presentation/theme/Color.kt
@@ -0,0 +1,21 @@
+package gradowska.katarzyna.filmsapp.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary
+val Tolopea = Color(0xFF21023D)
+
+// Basics
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
+// App colors
+val Gold = Color(0xFFFFD700)
+val Tamarillo = Color(0xFF8C1412)
+val WineBerry = Color(0xFF6C1E3B)
+val WineBerry2 = Color(0xFF471528)
+
+// Backgrounds / dark tones
+val Haiti = Color(0xFF15133F)
+val Haiti2 = Color(0xFF151439)
+val EnergyYellow = Color(0xFFF8DD5C)
